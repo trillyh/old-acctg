a7dd50a2a0b04b5c423b8e8e67fc6080f78c1f9e
Tung <trillionhoang0308@gmail.com>
2024-06-24 10:10:13 -0700

    Init tables

--- accounting.py
+++ accounting.py
@@ -13,12 +13,9 @@ connection = psycopg2.connect(host="localhost",
                               dbname="postgres",
                               user="postgres",
                               password="8383")
-
 cur = connection.cursor()
 
-connection.commit()
-
-user_table = """ User (
+user_table_def = """ (
   UserID SERIAL PRIMARY KEY,
   Username VARCHAR(255) UNIQUE NOT NULL,
   EMAIL VARCHAR(255) UNIQUE NOT NULL
@@ -26,32 +23,43 @@ user_table = """ User (
 """
 
 # Note: CreatedAt will not perform any timezone conversion
-journal_entry = """ JournalEntry (
+journal_entry_def = """ (
   EntryID SERIAL PRIMARY KEY,
   UserID INTEGER NOT NULL,
   TransactionDate DATE NOT NULL,
   Description VARCHAR(255),
   CreateAt TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
-  FOREIGN KEY (UserID) REFERENCES User(UserID)
+  FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 """
 
-journal_entry_account =  """ JournalEntryAccount(
+journal_entry_account_def =  """ (
   EntryAccountID SERIAL PRIMARY KEY,
   EntryID INTEGER NOT NULL,
-  AccountType 
+  Type VARCHAR(255) CHECK (Type IN ('Debit', 'Credit')),
+  Account VARCHAR(255) NOT NULL,
+  Amount DECIMAL(15, 2) NOT NULL CHECK(Amount >= 0),
+  FOREIGN KEY (EntryID) REFERENCES JournalEntry(EntryID)
 );
 """
 
-check_and_create_query = """ 
+tables_query = [
+    ("Users", user_table_def),
+    ("JournalEntry", journal_entry_def),
+    ("JournalEntryAccount", journal_entry_account_def)
+]
+
+try:
+   for table_name, table_definition in tables_query:
+     create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} {table_definition} "
+     cur.execute(create_table_query)
+   connection.commit()
+   print("Tables created successfully")
+except psycopg2.Error as e:
+   print("An error occurred: ", e)
+   connection.rollback()  # Roll back the transaction on error
 
 
-"""
 
-cur.execute()
-
-connection.commit()
-
-print("Close")
 cur.close()
 connection.close()
